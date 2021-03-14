f36cfeb666df1014d746371e8968834122b79670
SungminPark <rnfxl92@naver.com>
2021-03-14 23:58:32 +0900

    [feat] RxDataSource

--- UnsplashClone/UnsplashClone/ViewModel/RxPhotoViewModel.swift
+++ UnsplashClone/UnsplashClone/ViewModel/RxPhotoViewModel.swift
@@ -10,4 +10,25 @@ import RxSwift
 import RxCocoa
 import RxDataSources
 
-typealias SectionModel = AnimatableSectionModel<Int, Photo>
+typealias PhotoSectionModel = AnimatableSectionModel<Int, Photo>
+
+class RxPhotoViewModel: CommonViewModel {
+    let dataSource: RxTableViewSectionedAnimatedDataSource<PhotoSectionModel> = {
+        let ds = RxTableViewSectionedAnimatedDataSource<PhotoSectionModel>(configureCell: { (dataSource, tableView, indexPath, photo) -> UITableViewCell in
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: PhotoTableViewCell.Identifier.reusableCell, for: indexPath) as? PhotoTableViewCell else {
+                return UITableViewCell()
+            }
+            
+            cell.configureCell(username: photo.username, sponsored: photo.sponsored, imageSize: cell.frame.size)
+            
+            return cell
+        })
+        
+        ds.canEditRowAtIndexPath = { _, _ in
+            return true
+        }
+        return ds
+    }()
+    
+    //TODO: - Action 및 datasource에 api 요청을 통해 photo 추가하기
+}
