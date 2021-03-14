78c2ae8138f1522e7cf2ab6de29fd13acb0f4c60
SungminPark <rnfxl92@naver.com>
2021-03-14 18:10:57 +0900

    [refactor] Rx로 photoRequest부분 구현

---
+++ UnsplashClone/UnsplashClone/Network/Service/PhotoApiType.swift
@@ -0,0 +1,22 @@
+//
+//  PhotoApiType.swift
+//  UnsplashClone
+//
+//  Created by 박성민 on 2021/03/14.
+//
+
+import Foundation
+import RxSwift
+
+protocol PhotoApiType {
+    
+    @discardableResult
+    func fetchRandomPhoto() -> Observable<Photo?>
+
+    @discardableResult
+    func fetchPhotos(page: Int, perPage: Int) -> Observable<[Photo]>
+
+    @discardableResult
+    func fetchSearchedPhotos(page: Int, perPage: Int, query: String) -> Observable<[Photo]>
+
+}
---
+++ UnsplashClone/UnsplashClone/Network/Service/UnsplashPhotoApi.swift
@@ -0,0 +1,69 @@
+//
+//  UnsplashPhotoApi.swift
+//  UnsplashClone
+//
+//  Created by 박성민 on 2021/03/14.
+//
+
+import Foundation
+import RxSwift
+
+class UnsplashPhotoApi: PhotoApiType {
+    private let urlSession = URLSession.shared
+    
+    @discardableResult
+    func fetchRandomPhoto() -> Observable<Photo?> {
+        guard let url = UnsplashEndPoint.randomPhoto.url else {
+            return Observable.error(NetworkError.urlNotSupport)
+        }
+        let request = URLRequest(url: url)
+        
+        return urlSession.rx
+            .data(request: request)
+            .map { data -> Photo? in
+                let decoder = JSONDecoder()
+                
+                return try
+                    decoder.decode(Photo.self, from: data)
+            }
+            .catchAndReturn(nil)
+    }
+    
+    @discardableResult
+    func fetchPhotos(page: Int, perPage: Int) -> Observable<[Photo]> {
+        guard let url = UnsplashEndPoint.photos(page: page, perPage: perPage).url else {
+            return Observable.error(NetworkError.urlNotSupport)
+        }
+        
+        let request = URLRequest(url: url)
+        
+        return urlSession.rx
+            .data(request: request)
+            .map { data -> [Photo] in
+                let decoder = JSONDecoder()
+                
+                return try decoder.decode([Photo].self, from: data)
+            }
+            .catchAndReturn([])
+        
+    }
+    
+    @discardableResult
+    func fetchSearchedPhotos(page: Int, perPage: Int, query: String) -> Observable<[Photo]> {
+        guard let url = UnsplashEndPoint.searchPhotos(page: page, perPage: perPage, query: query).url else {
+            return Observable.error(NetworkError.urlNotSupport)
+        }
+        
+        let request = URLRequest(url: url)
+        
+        return urlSession.rx
+            .data(request: request)
+            .map { data -> [Photo] in
+                let decoder = JSONDecoder()
+                let searchedPhotos = try decoder.decode(SearchedPhoto.self, from: data)
+                
+                return searchedPhotos.results
+            }
+            .catchAndReturn([])
+    }
+}
